8ee61fe96d754ff6d21cb32d34183611afa8877f
weikengchen <w.k@berkeley.edu>
2024-05-05 23:33:12 +0800

    example for sha txout

--- src/script_pub_key.rs
+++ src/script_pub_key.rs
@@ -1,6 +1,29 @@
-use bitcoin::opcodes::all::{OP_PUSHBYTES_1, OP_PUSHBYTES_20, OP_PUSHBYTES_32};
+use bitcoin::opcodes::all::{
+    OP_PUSHBYTES_20, OP_PUSHBYTES_22, OP_PUSHBYTES_32, OP_PUSHBYTES_34, OP_PUSHNUM_1,
+};
 use bitcoin::opcodes::OP_0;
 use bitvm::treepp::*;
+
+pub enum ScriptPubKey {
+    P2WPKH(Vec<u8>),
+    P2WSH(Vec<u8>),
+    P2TR(Vec<u8>),
+}
+
+#[derive(Clone, Copy)]
+pub enum ScriptPubKeyType {
+    P2WPKH,
+    P2WSH,
+    P2TR,
+}
+
+pub const fn script_pub_key_len(script_pub_key_type: ScriptPubKeyType) -> usize {
+    match script_pub_key_type {
+        ScriptPubKeyType::P2WPKH => 2 + 20,
+        ScriptPubKeyType::P2WSH => 2 + 32,
+        ScriptPubKeyType::P2TR => 2 + 32,
+    }
+}
 
 pub struct ScriptPubKeyGadget;
 
@@ -8,7 +31,11 @@ impl ScriptPubKeyGadget {
     pub fn p2wpkh_from_constant_hash(pkhash: &[u8]) -> Script {
         assert_eq!(pkhash.len(), 20);
 
-        let mut script = vec![OP_0.to_u8(), OP_PUSHBYTES_20.to_u8()];
+        let mut script = vec![
+            OP_PUSHBYTES_22.to_u8(),
+            OP_0.to_u8(),
+            OP_PUSHBYTES_20.to_u8(),
+        ];
         script.extend_from_slice(pkhash);
         Script::from_bytes(script)
     }
@@ -16,7 +43,11 @@ impl ScriptPubKeyGadget {
     pub fn p2wsh_from_constant_hash(script_hash: &[u8]) -> Script {
         assert_eq!(script_hash.len(), 32);
 
-        let mut script = vec![OP_0.to_u8(), OP_PUSHBYTES_32.to_u8()];
+        let mut script = vec![
+            OP_PUSHBYTES_34.to_u8(),
+            OP_0.to_u8(),
+            OP_PUSHBYTES_32.to_u8(),
+        ];
         script.extend_from_slice(script_hash);
         Script::from_bytes(script)
     }
@@ -24,7 +55,11 @@ impl ScriptPubKeyGadget {
     pub fn p2tr_from_public_key(public_key: &[u8]) -> Script {
         assert_eq!(public_key.len(), 32);
 
-        let mut script = vec![OP_PUSHBYTES_1.to_u8(), OP_PUSHBYTES_32.to_u8()];
+        let mut script = vec![
+            OP_PUSHBYTES_34.to_u8(),
+            OP_PUSHNUM_1.to_u8(),
+            OP_PUSHBYTES_32.to_u8(),
+        ];
         script.extend_from_slice(public_key);
         Script::from_bytes(script)
     }
--- src/tx_out.rs
+++ src/tx_out.rs
@@ -1,4 +1,5 @@
 use crate::cpp_integer::CppUInt64Gadget;
+use crate::script_pub_key::{ScriptPubKey, ScriptPubKeyGadget, ScriptPubKeyType};
 use crate::variable_integer::VariableIntegerGadget;
 use bitcoin::consensus::Encodable;
 use bitcoin::opcodes::all::{
@@ -69,5 +70,130 @@ impl TxOutGadget {
 
     pub fn step_2_add_provided_small_bitcoin_integer() -> Script {
         VariableIntegerGadget::encode_small_bitcoin_number()
+    }
+
+    pub fn step_3_add_constant_script_pub_key(script_pub_key: &ScriptPubKey) -> Script {
+        match script_pub_key {
+            ScriptPubKey::P2WPKH(pkhash) => ScriptPubKeyGadget::p2wpkh_from_constant_hash(pkhash),
+            ScriptPubKey::P2WSH(script_hash) => {
+                ScriptPubKeyGadget::p2wsh_from_constant_hash(script_hash)
+            }
+            ScriptPubKey::P2TR(public_key) => ScriptPubKeyGadget::p2tr_from_public_key(public_key),
+        }
+    }
+
+    pub fn step_3_add_provided_script_pub_key(script_pub_key_type: ScriptPubKeyType) -> Script {
+        match script_pub_key_type {
+            ScriptPubKeyType::P2WPKH => script! {
+                OP_DUP OP_SIZE 20 OP_EQUALVERIFY
+            },
+            ScriptPubKeyType::P2WSH => script! {
+                OP_DUP OP_SIZE 32 OP_EQUALVERIFY
+            },
+            ScriptPubKeyType::P2TR => script! {
+                OP_DUP OP_SIZE 32 OP_EQUALVERIFY
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::script_pub_key::{script_pub_key_len, ScriptPubKey, ScriptPubKeyType};
+    use crate::tx_out::TxOutGadget;
+    use bitcoin::consensus::Encodable;
+    use bitcoin::hashes::Hash;
+    use bitcoin::key::TweakedPublicKey;
+    use bitcoin::secp256k1::Secp256k1;
+    use bitcoin::{Amount, ScriptBuf, TxOut, WPubkeyHash, WScriptHash, XOnlyPublicKey};
+    use bitvm::treepp::*;
+    use rand::{RngCore, SeedableRng};
+    use rand_chacha::ChaCha20Rng;
+    use sha2::digest::Update;
+    use sha2::{Digest, Sha256};
+
+    #[test]
+    fn test_tx_out() {
+        let mut prng = ChaCha20Rng::seed_from_u64(0);
+
+        for _ in 0..10 {
+            let value_1 = Amount::from_sat(prng.next_u64());
+            let value_2 = Amount::from_sat(prng.next_u64());
+            let value_3 = Amount::from_sat(prng.next_u64());
+
+            let mut pkhash = vec![0u8; 20];
+            prng.fill_bytes(&mut pkhash);
+            let script_pub_key_1 = ScriptPubKey::P2WPKH(pkhash.clone());
+
+            let mut script_hash = vec![0u8; 32];
+            prng.fill_bytes(&mut script_hash);
+            let script_pub_key_2 = ScriptPubKey::P2WSH(script_hash.clone());
+
+            let secp = Secp256k1::new();
+            let keypair = secp.generate_keypair(&mut prng);
+
+            let pubkey = XOnlyPublicKey::from(keypair.1);
+            let script_pub_key_3 = ScriptPubKey::P2TR(pubkey.serialize().to_vec());
+
+            let expected = {
+                let mut bytes = vec![];
+
+                let tx_out_1 = TxOut {
+                    value: value_1,
+                    script_pubkey: ScriptBuf::new_p2wpkh(
+                        &WPubkeyHash::from_slice(&pkhash).unwrap(),
+                    ),
+                };
+                let tx_out_2 = TxOut {
+                    value: value_2,
+                    script_pubkey: ScriptBuf::new_p2wsh(
+                        &WScriptHash::from_slice(&script_hash).unwrap(),
+                    ),
+                };
+                let tx_out_3 = TxOut {
+                    value: value_3,
+                    script_pubkey: ScriptBuf::new_p2tr_tweaked(
+                        TweakedPublicKey::dangerous_assume_tweaked(pubkey),
+                    ),
+                };
+
+                tx_out_1.consensus_encode(&mut bytes).unwrap();
+                tx_out_2.consensus_encode(&mut bytes).unwrap();
+                tx_out_3.consensus_encode(&mut bytes).unwrap();
+
+                let mut sha256 = Sha256::new();
+                Update::update(&mut sha256, &bytes);
+
+                let hash = sha256.finalize().to_vec();
+                hash
+            };
+
+            let script = script! {
+                { TxOutGadget::step_1_add_constant_value(value_1) }
+                { TxOutGadget::step_2_add_constant_vi(script_pub_key_len(ScriptPubKeyType::P2WPKH)) }
+                OP_CAT
+                { TxOutGadget::step_3_add_constant_script_pub_key(&script_pub_key_1) }
+                OP_CAT
+                { TxOutGadget::step_1_add_constant_value(value_2) }
+                OP_CAT
+                { TxOutGadget::step_2_add_constant_vi(script_pub_key_len(ScriptPubKeyType::P2WSH)) }
+                OP_CAT
+                { TxOutGadget::step_3_add_constant_script_pub_key(&script_pub_key_2) }
+                OP_CAT
+                { TxOutGadget::step_1_add_constant_value(value_3) }
+                OP_CAT
+                { TxOutGadget::step_2_add_constant_vi(script_pub_key_len(ScriptPubKeyType::P2TR)) }
+                OP_CAT
+                { TxOutGadget::step_3_add_constant_script_pub_key(&script_pub_key_3) }
+                OP_CAT
+                OP_SHA256
+
+                { expected }
+                OP_EQUAL
+            };
+
+            let exec_result = execute_script(script);
+            assert!(exec_result.success);
+        }
     }
 }
