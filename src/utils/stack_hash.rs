fe5a1ee0d89b5c7bd643e64907e96f902ac90c56
weikengchen <w.k@berkeley.edu>
2024-07-12 01:12:22 +0800

    make it possible to compute the stack hash from outside

--- src/utils/stack_hash.rs
+++ src/utils/stack_hash.rs
@@ -1,5 +1,8 @@
 use crate::treepp::*;
 use crate::utils::pseudo::OP_HINT;
+use bitcoin_scriptexec::utils::scriptint_vec;
+use sha2::digest::Update;
+use sha2::{Digest, Sha256};
 
 /// Gadget for hashing the stack.
 ///
@@ -7,6 +10,25 @@ use crate::utils::pseudo::OP_HINT;
 pub struct StackHash;
 
 impl StackHash {
+    /// Compute the hash outside the Bitcoin script
+    pub fn compute(v: &[Vec<u8>]) -> Vec<u8> {
+        let len = v.len();
+
+        let mut sha256 = Sha256::new();
+        let mut hash = {
+            Update::update(&mut sha256, &scriptint_vec(len as i64));
+            Digest::finalize_reset(&mut sha256).to_vec()
+        };
+
+        for elem in v.iter().rev() {
+            Update::update(&mut sha256, elem);
+            Update::update(&mut sha256, &hash);
+            hash = Digest::finalize_reset(&mut sha256).to_vec()
+        }
+
+        hash
+    }
+
     /// Hashing the stack elements and drop them
     pub fn hash_drop(num: usize) -> Script {
         assert!(num > 0);
@@ -50,6 +72,7 @@ impl StackHash {
 mod test {
     use crate::treepp::*;
     use crate::utils::stack_hash::StackHash;
+    use bitcoin_scriptexec::utils::scriptint_vec;
     use rand::{Rng, SeedableRng};
     use rand_chacha::ChaCha20Rng;
 
@@ -110,6 +133,51 @@ mod test {
                 OP_EQUALVERIFY
 
                 for _ in 0..r.len() {
+                    OP_DROP
+                }
+
+                OP_TRUE
+            };
+
+            let exec_result = execute_script(script);
+            assert!(exec_result.success);
+        }
+    }
+
+    #[test]
+    fn test_compute_hash() {
+        let mut prng = ChaCha20Rng::seed_from_u64(0);
+
+        for i in 2..=20 {
+            let mut v = vec![];
+            for _ in 0..i {
+                v.push(prng.gen::<u16>() as u32);
+            }
+
+            let mut r = vec![]; // rubbish data
+            for _ in 0..i {
+                r.push(prng.gen::<u16>() as u32);
+            }
+
+            let computed_hash = StackHash::compute(
+                &v.iter()
+                    .map(|x| scriptint_vec(*x as i64))
+                    .collect::<Vec<Vec<u8>>>(),
+            );
+
+            let script = script! {
+                for elem in v.iter() {
+                    { *elem }
+                }
+                for elem in r.iter() {
+                    { *elem }
+                }
+
+                { StackHash::hash_from_hint(i) }
+                { computed_hash }
+                OP_EQUALVERIFY
+
+                for _ in 0..(v.len() + r.len()) {
                     OP_DROP
                 }
 
